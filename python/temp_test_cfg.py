b1dc656539ec075616f6233e246c316f40ea869f
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-15 02:17:08 +0530

    Create temp_test_cfg.py

---
+++ python/temp_test_cfg.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+process = cms.Process("FramesProducer")
+
+#process.source = cms.Source("EmptySource")
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring("file:/afs/cern.ch/user/s/schaudha/public/CMSSW_10_6_8/src/demo/DoublePhotonPt10To100_pythia8_ReAOD_PU2017_MINIAODSIM.root")
+    , skipEvents = cms.untracked.uint32(0)#options.skipEvents
+    )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+#load our module
+#module stuff = MyStuffProducer {}
+#process.stuff=cms.EDProducer("MyStuffProducer") 
+
+process.stuff = cms.EDProducer('MyStuffProducer'
+    , reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
+    , photonCollection = cms.InputTag('slimmedPhotons'))
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("myoutput1.root")#options.outputFile
+    )
+
+#make sure our module is called every event
+process.p = cms.Path(process.stuff)
+
+#just to see that something is happening
+#service = cms.Tracer()
