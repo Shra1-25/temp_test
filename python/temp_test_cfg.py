7723c5d6bf8d9602604db97b2721ebe6bfeb2edb
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-15 02:28:31 +0530

    Update temp_test_cfg.py

--- python/temp_test_cfg.py
+++ python/temp_test_cfg.py
@@ -7,7 +7,7 @@ process = cms.Process("temptest")
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring("file:/afs/cern.ch/user/s/schaudha/public/CMSSW_10_6_8/src/demo/DoublePhotonPt10To100_pythia8_ReAOD_PU2017_MINIAODSIM.root")
     , skipEvents = cms.untracked.uint32(0)#options.skipEvents
     )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 #load our module
 #module stuff = MyStuffProducer {}
@@ -15,7 +15,12 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.stuff = cms.EDProducer('temp_test'
     , reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
-    , photonCollection = cms.InputTag('slimmedPhotons'))
+    , photonCollection = cms.InputTag('slimmedPhotons')
+    , integer_=cms.InputTag('integer')
+    , value_=cms.InputTag('value')
+    , vecvalues_=cms.InputTag('vecvalues')
+    , tempgenParticles_=cms.InputTag('tempgenParticles')
+    )
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string("myoutput1.root")#options.outputFile
